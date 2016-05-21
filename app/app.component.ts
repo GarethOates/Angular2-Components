210a0af5839abf477b704ae5c3e5f621688979b6
Gareth Oates <goatie@gmail.com>
2016-05-21 15:24:53 +0200

    Added Subscribe-form component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from 'angular2/core';
-import { ContactFormComponent } from './contact-form.component'
+import { SubscribeFormComponent } from './subscribe-form.component'
 
 @Component({
     selector: 'my-app',
     templateUrl: "app/app.component.html",
-    directives: [ContactFormComponent]
+    directives: [SubscribeFormComponent]
 })
 export class AppComponent {
 }
