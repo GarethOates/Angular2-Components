037fb98d719bea431bf0a678c4730fe85277b67f
Gareth Oates <goatie@gmail.com>
2016-09-09 00:42:43 +0200

    Linting

--- app/github.component.ts
+++ app/github.component.ts
@@ -1,25 +1,25 @@
-import { Component, OnInit } from 'angular2/core'
-import { GitHubService } from './github.service'
-import { HTTP_PROVIDERS } from 'angular2/http'
-import { Observable } from 'rxjs/Rx'
+import { Component, OnInit } from "angular2/core";
+import { GitHubService } from "./github.service";
+import { HTTP_PROVIDERS } from "angular2/http";
+import { Observable } from "rxjs/Rx";
 
 @Component({
-    selector: 'gitHub',
-    templateUrl: 'app/github.component.html',
-    styleUrls: ['app/github.component.css'],
+    selector: "gitHub",
+    templateUrl: "app/github.component.html",
+    styleUrls: ["app/github.component.css"],
     providers: [GitHubService, HTTP_PROVIDERS]
 })
 export class GitHubComponent implements OnInit {
-    
+
     isLoading: boolean = true;
     User;
     Followers;
-    
-    username = "Octocat";
-    
+
+    username = "GarethOates";
+
     constructor(private gitHubService: GitHubService) { }
-    
-    ngOnInit() {       
+
+    ngOnInit() {
         Observable.forkJoin(
             this.gitHubService.getUserData(this.username),
             this.gitHubService.getFollowers(this.username)
@@ -27,7 +27,7 @@ export class GitHubComponent implements OnInit {
             res => {
                 this.User = res[0];
                 this.Followers = res[1];
-            }, null, () => { this.isLoading = false }
+            }, null, () => { this.isLoading = false; }
         );
     }
 }
--- app/github.service.ts
+++ app/github.service.ts
@@ -1,24 +1,25 @@
-import { Injectable } from 'angular2/core'
-import { Http } from 'angular2/http'
+import { Injectable } from "angular2/core";
+import { Http } from "angular2/http";
 
-import { Observable } from 'rxjs/Rx'
-import 'rxjs/add/operator/map'
+import { Observable } from "rxjs/Rx";
+import "rxjs/add/operator/map";
 
 @Injectable()
 export class GitHubService {
-    
+
     private url: string;
-    
+
     constructor(private _http: Http) {
         this.url = "https://api.github.com/users/";
     }
-    
+
     getUserData(name: string) {
-        return this._http.get(this.url + name).map(response => response.json());
+        return this._http.get(this.url + name)
+            .map(response => response.json());
     }
-    
+
     getFollowers(name: string) {
-        return this._http.get(this.url + name + "/followers").map(response => response.json());
+        return this._http.get(this.url + name + "/followers")
+            .map(response => response.json());
     }
-    
 }
