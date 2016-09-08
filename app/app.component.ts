55d71f2cb5451d31e87414922b9a2f7868441eff
Gareth Oates <goatie@gmail.com>
2016-09-09 00:46:45 +0200

    More Linting

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from 'angular2/core';
-import { GitHubComponent } from './github.component'
+import { Component } from "angular2/core";
+import { GitHubComponent } from "./github.component";
 
 @Component({
-    selector: 'my-app',
+    selector: "my-app",
     templateUrl: "app/app.component.html",
     directives: [GitHubComponent]
 })
 export class AppComponent {
-    
+
 }
